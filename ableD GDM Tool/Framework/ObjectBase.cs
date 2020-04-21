f32f34c57e5976c050072731286f4bf78e16f97d
hari krishna <harikrishna2005@gmail.com>
2020-04-21 20:22:49 +0530

    INotifyPropertyChanged Class completed. Need to test

---
+++ ableD GDM Tool/Framework/ObjectBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ableD_GDM_Tool.Framework
+{
+    public class ObjectBase : INotifyPropertyChanged
+    {
+        /// <summary>
+        ///     Property Changing Notification 
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            /* if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+            */
+
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
