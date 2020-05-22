f08681d70d9a638f595a296e1416971d69d209ab
Ambati <harikrishna2005@gmail.com>
2020-05-22 19:26:24 +0530

    tkt class with properties

--- ableD GDM Tool/Common/Models/MediaType.cs
+++ ableD GDM Tool/Common/Models/MediaType.cs
@@ -1,21 +1,43 @@
 ﻿using System.Security.Cryptography;
+using System.Windows;
 
 namespace ableD_GDM_Tool.Common.Models
 {
     public abstract class MediaType : IMediatype
     {
-        
-        
+        public string MediaTypeName { get; set; }
+        public abstract string GetReadData();
+
     }
 
-    public class Tkt
+    public class Tkt : MediaType
     {
+        public int Length { get; set; }
+        public string AirlineCode { get; set; }
+        public string IssueDate { get; set; }
+        public string PnrRl { get; set; }
+        public string TktRl { get; set; }
+        public string ConfirmationNumber { get; set; }
+        public string Spare { get; set; } = "   ";
+        public string Identification { get; set; }
+        public string IssueTime { get; set; }
         
+        
+        
+        public override string GetReadData()
+        { 
+            
+            
+            string.Concat(AirlineCode, IssueDate, PnrRl, TktRl, ConfirmationNumber, Spare, Identification, IssueTime);
+            return "super";
+
+
+        }
     }
 
     public interface IMediatype
     {
-        
-        
+       string MediaTypeName { get; set; }
+       string GetReadData();
     }
 }
